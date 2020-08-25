76117754b964da332deed916ebd7e6e520d4c4a4
Todd Tyler <twtyler@gmail.com>
2020-08-25 17:09:01 -0600

    Adding models.

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +8,25 @@ namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:5000");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+
+
         public async Task Run()
         {
-        
+            await GetResource();
+        }
+
+
+        public async Task GetResource()
+        {
+            var response = await _httpClient.GetAsync("api/movies");
+            response.EnsureSuccessStatusCode();
         }
     }
 }
