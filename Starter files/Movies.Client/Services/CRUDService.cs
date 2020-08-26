13dbe847cbec35e45d61f31e4e01ba7e91321a7b
Todd Tyler <twtyler@gmail.com>
2020-08-25 21:42:28 -0600

    Adding GetResource

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Movies.Client.Services
 {
@@ -14,12 +20,18 @@ namespace Movies.Client.Services
         {
             _httpClient.BaseAddress = new Uri("http://localhost:5000");
             _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+
+            Thread.Sleep(9000);
         }
 
 
         public async Task Run()
         {
-            await GetResource();
+            // await GetResource();
+            await GetResourceThroughHttpRequestMessage();
         }
 
 
@@ -27,6 +39,36 @@ namespace Movies.Client.Services
         {
             var response = await _httpClient.GetAsync("api/movies");
             response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var movies = new List<Movie>();
+
+            if (response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+            }
+            else if (response.Content.Headers.ContentType.MediaType == "application/xml")
+            {
+                var serializer = new XmlSerializer(typeof(List<Movie>));
+                movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
+            }
+        }
+
+        public async Task GetResourceThroughHttpRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+        }
+
+
+        public async Task CreateResource()
+        {
+
         }
     }
 }
